59df13af6de55fb6413b63df228f29207f636ed4
Smith <pentopia11@gmail.com>
2022-05-26 00:02:24 -0700

    added images and started on bot code

--- bot.js
+++ bot.js
@@ -1,15 +1,26 @@
 // Loading dependencies
-const Discord = require("discord.js");
-const config = require('./config.json');
+const { Client, Intents, Collection, MessageEmbed } = require('discord.js');
 const auth = require('./auth.json');
 const flagbot = require('./flagbot.json');
 // Create a new bot object
-const client = new Discord.Client();
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		// Intents.FLAGS.GUILD_PRESENCES
+	],
+	partials: ['MESSAGE', 'REACTION'],
+});
 
 // Loading global variables
-//var allData = JSON.parse(JSON.stringify(flagbot));
+var allData = JSON.parse(JSON.stringify(flagbot));
 
-//console.log(allData["1"].name);
+items = Object.keys(allData);
+//.forEach((item, i) => console.log(allData[item].name));
+
+var aliasList = new Map();
+
 
 
 
@@ -23,10 +34,25 @@ client.once('ready', () => {
 
 client.on('message', function (m) {
 	// bot does not reply to itself
-	if (mm.author.bot) return;
+	if (m.author.bot) return;
+	
+	let ml = m.content.toLowerCase();
+	
+	if (ml == "^help") help(m.channel);
 
 	
+	
 });
+
+function help(ch) {
+	const emb = new MessageEmbed()
+		.setTitle('Help Guide!')
+		.setColor('#67b4c2')
+		.addField('Commands', '--------------------------------------')
+		.addField('^help', "Displays help.", true)
+		.setTimestamp();
+	ch.send({ embeds: [emb] });
+}
 
 client.on("messageReactionAdd", async (reaction, user) => {
 	return;
