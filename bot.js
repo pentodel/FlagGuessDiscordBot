99325ae476d44e5d62d4dd047d6eafbc2e2520e0
Smith <pentopia11@gmail.com>
2022-05-26 02:03:40 -0700

    started working on data command, now debugging

--- bot.js
+++ bot.js
@@ -21,23 +21,13 @@ items = Object.keys(allData);
 
 var aliasList = new Map();
 
-items.forEach((item, i) =>
-	if (allData[item].alias1 != "") {
-		aliasList.set(allData[item].alias1, item);
+items.forEach((item) => {
+	if (Array.isArray(allData[item].alias)) {
+		allData[item].alias.forEach((al) => {
+			aliasList.set(al,item);
+		});
 	}
-	if (allData[item].alias2 != "") {
-		aliasList.set(allData[item].alias2, item);
-	}
-	if (allData[item].alias3 != "") {
-		aliasList.set(allData[item].alias3, item);
-	}
-	if (allData[item].alias4 != "") {
-		aliasList.set(allData[item].alias4, item);
-	}
-	if (allData[item].alias5 != "") {
-		aliasList.set(allData[item].alias5, item);
-	}
-);
+});
 
 
 
@@ -57,17 +47,58 @@ client.on('message', function (m) {
 	let ml = m.content.toLowerCase();
 	
 	if (ml == "^help") help(m.channel);
-
+	
+	let messagewords = ml.split(" ");
+	
+	if (messagewords[0] == "^data") {
+		messagewords.shift(); // remove the first item
+		let alias = messagewords.join(" ");
+		let id = getid(alias);
+		if (id == undefined) {
+			m.channel.send("Country not found!");
+			return;
+		}
+		countrydata(id, m.channel);
+		return;
+	}
 	
 	
 });
 
 function help(ch) {
-	const emb = new MessageEmbed()
+	let emb = new MessageEmbed()
 		.setTitle('Help Guide!')
 		.setColor('#67b4c2')
 		.addField('Commands', '--------------------------------------')
 		.addField('^help', "Displays help.", true)
+		.setTimestamp();
+	ch.send({ embeds: [emb] });
+}
+
+function getid(alias) {
+	console.log("-" + alias + "-");
+	console.log(aliasList);
+	console.log(aliasList[alias]);
+	return aliasList[alias];
+}
+
+function countrydata(id, ch) {
+	let country = allData[id];
+	let aliases = "None.";
+	if (country.aliases != "") {
+		aliases = "";
+		country.aliases.forEach((e, i) => {
+			aliases += e + ", ";
+		});
+	}
+	let emb = new MessageEmbed()
+		.setTitle(country.name)
+		.setColor('#000000')
+		.setImage(country.url)
+		.addField('Sovereign', country.sovereign, true)
+		.addField('Continent', country.continent, true)
+		.addField('Subregion', country.subregion, true)
+		.addfield('Aliases', aliases, true)
 		.setTimestamp();
 	ch.send({ embeds: [emb] });
 }
